38a4daa595680ef294d327dd80cae925b670d76c
JustBulky <geofforyeide@gmail.com>
2026-02-26 11:34:55 -0600

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+  earlyAccess: true,
   migrations: {
     path: "prisma/migrations",
+    url: process.env.DATABASE_URL!,
   },
   datasource: {
     url: process.env.DATABASE_URL!,
